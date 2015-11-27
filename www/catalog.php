c2ef2f6eb1731b78404ee3c3b90b030b868b0e77
Padavan <padavan94@gmail.com>
2015-11-27 18:38:08 +0200

    123

--- www/catalog.php
+++ www/catalog.php
@@ -1,4 +1,4 @@
-<?php include("templates/header.php") ?>
+﻿<?php include("templates/header.php") ?>
 <?php include("templates/navigation.php") ?>
 <?php include("templates/main-navigation.php") ?>
 <div class="main-wrapper clearfix">
@@ -7,107 +7,279 @@
 		<div class="catalog-wrapper clearfix">
 			<div class="col-md-3 left-sidebar">
 				<ul class="sidebar" id="menu">
+					
 					<li class="">
-						<a href="#" aria-expanded="true">
-							<span class="sidebar-nav-item-icon fa fa-github fa-lg"></span>
-							<span class="sidebar-nav-item">metisMenu</span>
-							<span class="fa arrow"></span>
-						</a>
-						<ul aria-expanded="false" class="collapse" style="height: 0px;">
-							<li>
-								<a href="https://github.com/onokumus/metisMenu">
-									<span class="sidebar-nav-item-icon fa fa-code-fork"></span>
-									Fork
-								</a>
-							</li>
-							<li>
-								<a href="https://github.com/onokumus/metisMenu">
-									<span class="sidebar-nav-item-icon fa fa-star"></span>
-									Star
-								</a>
-							</li>
-							<li>
-								<a href="https://github.com/onokumus/metisMenu/issues">
-									<span class="sidebar-nav-item-icon fa
-									fa-exclamation-triangle"></span>
-									Issues
-								</a>
-							</li>
-						</ul>
-					</li>
-					<li class="">
-						<a href="#" aria-expanded="false">Menu 0 <span class="fa
+						<a href="#" aria-expanded="false">Sed ut perspiciatis unde<span class="fa
 						arrow"></span></a>
-						<ul aria-expanded="false" class="collapse" style="height: 0px;">
-							<li><a href="#">item 0.1</a></li>
-							<li><a href="#">item 0.2</a></li>
-							<li><a href="http://onokumus.com">onokumus</a></li>
-							<li><a href="#">item 0.4</a></li>
+						<ul aria-expanded="false" class="collapse items">
+							<li><a href="#">asdasd</a></li>
+							<li><a href="#">asdasd</a></li>
+							<li><a href="#">asdasd</a></li>
+							<li><a href="#">asdasd</a></li>
+							<li><a href="#">asdasd</a></li>
 						</ul>
 					</li>
 					<li class="">
 						<a href="#" aria-expanded="false">Menu 1 <span class="glyphicon
 						arrow"></span></a>
-						<ul aria-expanded="false" class="collapse" style="height: 0px;">
-							<li><a href="#">item 1.1</a></li>
-							<li><a href="#">item 1.2</a></li>
-							<li class="">
-								<a href="#" aria-expanded="true">Menu 1.3 <span class="fa
-								plus-times"></span></a>
-								<ul aria-expanded="false" class="collapse" style="height: 0px;">
-									<li><a href="#">item 1.3.1</a></li>
-									<li><a href="#">item 1.3.2</a></li>
-									<li><a href="#">item 1.3.3</a></li>
-									<li><a href="#">item 1.3.4</a></li>
-								</ul>
-							</li>
-							<li><a href="#">item 1.4</a></li>
-							<li class="">
-								<a href="#" aria-expanded="false">Menu 1.5 <span class="fa
-								plus-minus"></span></a>
-								<ul aria-expanded="false" class="collapse" style="height: 0px;">
-									<li><a href="#">item 1.5.1</a></li>
-									<li><a href="#">item 1.5.2</a></li>
-									<li><a href="#">item 1.5.3</a></li>
-									<li><a href="#">item 1.5.4</a></li>
-								</ul>
-							</li>
+						<ul aria-expanded="false" class="collapse items" >
+							<li><a href="#">asdasd</a></li>
+							<li><a href="#">asdasd</a></li>
+							<li><a href="#">asdasd</a></li>
+							<li><a href="#">asdasd</a></li>
+							<li><a href="#">asdasd</a></li>
 						</ul>
 					</li>
 					<li class="">
 						<a href="#" aria-expanded="true">Menu 2 <span class="glyphicon
 						arrow"></span></a>
-						<ul aria-expanded="false" class="collapse" style="height: 0px;">
-							<li><a href="#">item 2.1</a></li>
-							<li><a href="#">item 2.2</a></li>
-							<li><a href="#">item 2.3</a></li>
-							<li><a href="#">item 2.4</a></li>
+						<ul aria-expanded="false" class="collapse items">
+							<li><a href="#">asdasd</a></li>
+							<li><a href="#">asdasd</a></li>
+							<li><a href="#">asdasd</a></li>
+							<li><a href="#">asdasd</a></li>
+							<li><a href="#">asdasd</a></li>
+						</ul>
+					</li>
+					<li class="">
+						<a href="#" aria-expanded="true">Menu 2 <span class="glyphicon
+						arrow"></span></a>
+						<ul aria-expanded="false" class="collapse items">
+							<li><a href="#">asdasd</a></li>
+							<li><a href="#">asdasd</a></li>
+							<li><a href="#">asdasd</a></li>
+							<li><a href="#">asdasd</a></li>
+							<li><a href="#">asdasd</a></li>
 						</ul>
 					</li>
 				</ul>
 			</div>
+
 			<div class="col-md-9 right-catalog">
 				<div class="top-filter-tape">
-					<a href="#" class="top-filter-tape__filter">
-						<img class="svg" src="img/kahel.svg" alt="svg">Список
-					</a>
-					<a href="#" class="top-filter-tape__filter">
-						<img class="svg" src="img/squares.svg" alt="svg">Плитка
-					</a>
-					<a href="#" class="top-filter-tape__filter">
-						По цене
-					</a>
-					<a href="#" class="top-filter-tape__filter">
-						По мощности
-					</a>
-					
+					<div  class="top-filter-tape__filter col-md-3">
+						<a href="#">
+							<img class="svg" src="img/kahel.svg" alt="svg"><span>Список</span>
+						</a>
+					</div>
+					<div  class="top-filter-tape__filter col-md-3">
+						<a href="#">
+							<img class="svg" src="img/squares.svg" alt="svg"><span>Плитка</span>
+						</a>
+					</div>
+					<div class="top-filter-tape__filter col-md-3">
+						<a href="#">
+							<img class="svg" src="img/dollar.svg" alt="dollar">
+							<span>По цене</span>
+						</a>
+						<!-- submenu start -->	
+						<span class="top-filter-tape__filter-menu">
+							<a href="#">от дешевого к дорогому</a>
+							<a href="#">от дорогого к дешевому</a>
+						</span>
+						<!-- submenu end -->
+					</div>
+					<div class="top-filter-tape__filter col-md-3">
+						<a href="#">
+							<span>По мощности</span>
+						</a>
+
+					</div>
 				</div>
 				<div class="top-filter-tape">
-					<div class="top-filter-tape__filter">asdas</div>
-					<div class="top-filter-tape__filter">asdas</div>
-					<div class="top-filter-tape__filter">asdas</div>
-					<div class="top-filter-tape__filter">asdas</div>
+					<div class="col-md-3 top-filter-tape__filter">
+						<a href="#">
+							<span>По объёму</span>
+						</a>
+					</div>
+					<div class="col-md-3 top-filter-tape__filter">
+						<a href="#">
+							<span>По мощности</span>
+						</a>
+					</div>
+					<div class="col-md-3 top-filter-tape__filter">
+						<a href="#">
+							<span>По чему то еще</span>
+						</a>
+					</div>
+					<div class="col-md-3 top-filter-tape__filter">
+						<a href="#">
+							<span>Товаров на странице</span>
+						</a>
+						<!-- submenu start -->	
+						<span class="top-filter-tape__filter-menu">
+							<a href="#">10</a>
+							<a href="#">20</a>
+							<a href="#">50</a>
+							<a href="#">100</a>
+						</span>
+						<!-- submenu end -->
+					</div>
 				</div>
+
+				<!-- product wrapper start -->	
+				<div class="product-tape-wrapper">
+					<div class="product-tape clearfix">
+						<!-- product box start -->
+						<div class="product-box clearfix">
+							<div class="product-box__image">
+								<img src="img/generator.png"  alt="generator">
+							</div>
+							<div class="product-box__description clearfix">
+								<div class="product-box__name ">
+									<span>Генератор миниэлектростанция ЕCMT 7000GV</span>
+								</div>
+								<div class="col-md-4 product-box__prices-wrapper">
+									<div class="product-box__old-price">
+										<span>8 550 грн.</span>
+									</div>
+									<div class="product-box__price">
+										<span>6 900 грн.</span>
+									</div>
+									<a href="#" class="product-box__item-order product-box__item-order--mod">
+										Заказать
+										<div class="phone">
+											<img class="svg" src="img/phone.svg" alt="phone">
+										</div>
+									</a>
+								</div>
+								<div class="col-md-8">
+									<span>Продукция промышленной группы - незаменимые помощники, сопровождающие Вас как на работе, так и на отдыхе. Для выработки тока или подачи воды...</span>
+								</div>
+
+								<a href="#" class="product-box__item-more product-box__item-more--mod">
+									<span>подробнее</span><i class="fa fa-angle-double-right"></i>
+								</a>
+							</div>
+						</div>
+						<!-- product box end -->
+						<!-- product box start -->
+						<div class="product-box clearfix">
+							<div class="product-box__image">
+								<img src="img/generator.png"  alt="generator">
+							</div>
+							<div class="product-box__description clearfix">
+								<div class="product-box__name ">
+									<span>Генератор миниэлектростанция ЕCMT 7000GV</span>
+								</div>
+								<div class="col-md-4 product-box__prices-wrapper">
+									<div class="product-box__old-price">
+										<span>8 550 грн.</span>
+									</div>
+									<div class="product-box__price">
+										<span>6 900 грн.</span>
+									</div>
+									<a href="#" class="product-box__item-order product-box__item-order--mod">
+										Заказать
+										<div class="phone">
+											<img class="svg" src="img/phone.svg" alt="phone">
+										</div>
+									</a>
+								</div>
+								<div class="col-md-8">
+									<span>Продукция промышленной группы - незаменимые помощники, сопровождающие Вас как на работе, так и на отдыхе. Для выработки тока или подачи воды...</span>
+								</div>
+
+								<a href="#" class="product-box__item-more product-box__item-more--mod">
+									<span>подробнее</span><i class="fa fa-angle-double-right"></i>
+								</a>
+							</div>
+						</div>
+						<!-- product box end -->
+						<!-- product box start -->
+						<div class="product-box clearfix">
+							<div class="product-box__image">
+								<img src="img/generator.png"  alt="generator">
+							</div>
+							<div class="product-box__description clearfix">
+								<div class="product-box__name ">
+									<span>Генератор миниэлектростанция ЕCMT 7000GV</span>
+								</div>
+								<div class="col-md-4 product-box__prices-wrapper">
+									<div class="product-box__old-price">
+										<span>8 550 грн.</span>
+									</div>
+									<div class="product-box__price">
+										<span>6 900 грн.</span>
+									</div>
+									<a href="#" class="product-box__item-order product-box__item-order--mod">
+										Заказать
+										<div class="phone">
+											<img class="svg" src="img/phone.svg" alt="phone">
+										</div>
+									</a>
+								</div>
+								<div class="col-md-8">
+									<span>Продукция промышленной группы - незаменимые помощники, сопровождающие Вас как на работе, так и на отдыхе. Для выработки тока или подачи воды...</span>
+								</div>
+
+								<a href="#" class="product-box__item-more product-box__item-more--mod">
+									<span>подробнее</span><i class="fa fa-angle-double-right"></i>
+								</a>
+							</div>
+						</div>
+						<!-- product box end -->
+						<!-- product box start -->
+						<div class="product-box clearfix">
+							<div class="product-box__image">
+								<img src="img/generator.png"  alt="generator">
+							</div>
+							<div class="product-box__description clearfix">
+								<div class="product-box__name ">
+									<span>Генератор миниэлектростанция ЕCMT 7000GV</span>
+								</div>
+								<div class="col-md-4 product-box__prices-wrapper">
+									<div class="product-box__old-price">
+										<span>8 550 грн.</span>
+									</div>
+									<div class="product-box__price">
+										<span>6 900 грн.</span>
+									</div>
+									<a href="#" class="product-box__item-order product-box__item-order--mod">
+										Заказать
+										<div class="phone">
+											<img class="svg" src="img/phone.svg" alt="phone">
+										</div>
+									</a>
+								</div>
+								<div class="col-md-8">
+									<span>Продукция промышленной группы - незаменимые помощники, сопровождающие Вас как на работе, так и на отдыхе. Для выработки тока или подачи воды...</span>
+								</div>
+
+								<a href="#" class="product-box__item-more product-box__item-more--mod">
+									<span>подробнее</span><i class="fa fa-angle-double-right"></i>
+								</a>
+							</div>
+						</div>
+						<!-- product box end -->
+
+					</div>
+					<nav>
+					  <ul class="pagination">
+					    <li>
+					      <a href="#" aria-label="Previous">
+					        <span aria-hidden="true">&laquo;</span>
+					      </a>
+					    </li>
+					    <li><a href="#">1</a></li>
+					    <li><a href="#">2</a></li>
+					    <li><a href="#">3</a></li>
+					    <li><a href="#">4</a></li>
+					    <li><a href="#">5</a></li>
+					    <li><a href="#">6</a></li>
+					    <li><a href="#">7</a></li>
+					    <li><a href="#">8</a></li>
+					    <li><a href="#">9</a></li>
+					    <li>
+					      <a href="#" aria-label="Next">
+					        <span aria-hidden="true">&raquo;</span>
+					      </a>
+					    </li>
+					  </ul>
+					</nav>
+				</div>
+				<!-- product wrapper end -->	
+				
 			</div>
 		</div>
 	</div>
--- www/templates/catalog-top-slider.php
+++ www/templates/catalog-top-slider.php
@@ -1,172 +1,148 @@
 ﻿<!-- top-catalog-slider start -->
 <div class="top-catalog-slider clearfix col-md-12">
 	<!-- slider-item start -->
-	<div class=" top-catalog-slider__item ">
-		<div class="status">
+	<div class=" top-catalog-slider__item">
+		<div class="top-catalog-slider__item-status">
 			<img src="img/hit.png" alt="hit">
 		</div>
-		<div class="photo">
+		<div class="top-catalog-slider__item-photo">
 			<img src="img/generator.png"  alt="generator">
 		</div>
-		<div class="name">
+		<div class="top-catalog-slider__item-name">
 			<span>Генератор /<br> миниэлектростанция <br> ЕCMT 7000GV</span>
 		</div>
-		<div class="old-price">
+		<div class="top-catalog-slider__item-old-price">
 			<span>8 550 грн.</span>
 		</div>
-		<div class="price">
+		<div class="top-catalog-slider__item-price">
 			<span>6 900 грн.</span>
 		</div>
-		<a href="#" class="order">
+		<a href="#" class="top-catalog-slider__item-order">
 			Заказать
 			<div class="phone">
 				<img class="svg" src="img/phone.svg" alt="phone">
 			</div>
 		</a>
-		<a href="#" class="more">
+		<a href="#" class="top-catalog-slider__item-more">
 			<span>подробнее</span><i class="fa fa-angle-double-right"></i>
 		</a>
 	</div>
 	<!-- slider-item end -->
 	<!-- slider-item start -->
-	<div class=" top-catalog-slider__item ">
-		<div class="status">
-			<img src="img/akcii.png" alt="akcii">
-		</div>
-		<div class="photo">
-			<img src="img/generator.png"  alt="generator">
-		</div>
-		<div class="name">
-			<span>Генератор /<br> миниэлектростанция <br> ЕCMT 7000GV</span>
-		</div>
-		<div class="old-price">
-			<span>8 550 грн.</span>
-		</div>
-		<div class="price">
-			<span>6 900 грн.</span>
-		</div>
-		<a href="#" class="order">
-			Заказать
-			<div class="phone">
-				<img class="svg" src="img/phone.svg" alt="phone">
-			</div>
-		</a>
-		<a href="#" class="more">
-			<span>подробнее</span><i class="fa fa-angle-double-right"></i>
-		</a>
-	</div>
-	<!-- slider-item end -->
-	<!-- slider-item start -->
-	<div class=" top-catalog-slider__item ">
-		<div class="status">
+	<div class=" top-catalog-slider__item">
+		<div class="top-catalog-slider__item-status">
 			<img src="img/hit.png" alt="hit">
 		</div>
-		<div class="photo">
+		<div class="top-catalog-slider__item-photo">
 			<img src="img/generator.png"  alt="generator">
 		</div>
-		<div class="name">
+		<div class="top-catalog-slider__item-name">
 			<span>Генератор /<br> миниэлектростанция <br> ЕCMT 7000GV</span>
 		</div>
-		<div class="old-price">
+		<div class="top-catalog-slider__item-old-price">
 			<span>8 550 грн.</span>
 		</div>
-		<div class="price">
+		<div class="top-catalog-slider__item-price">
 			<span>6 900 грн.</span>
 		</div>
-		<a href="#" class="order">
+		<a href="#" class="top-catalog-slider__item-order">
 			Заказать
 			<div class="phone">
 				<img class="svg" src="img/phone.svg" alt="phone">
 			</div>
 		</a>
-		<a href="#" class="more">
+		<a href="#" class="top-catalog-slider__item-more">
 			<span>подробнее</span><i class="fa fa-angle-double-right"></i>
 		</a>
 	</div>
 	<!-- slider-item end -->
 	<!-- slider-item start -->
-	<div class=" top-catalog-slider__item ">
-		<div class="status">
-			<img src="img/akcii.png" alt="akcii">
-		</div>
-		<div class="photo">
-			<img src="img/generator.png"  alt="generator">
-		</div>
-		<div class="name">
-			<span>Генератор /<br> миниэлектростанция <br> ЕCMT 7000GV</span>
-		</div>
-		<div class="old-price">
-			<span>8 550 грн.</span>
-		</div>
-		<div class="price">
-			<span>6 900 грн.</span>
-		</div>
-		<a href="#" class="order">
-			Заказать
-			<div class="phone">
-				<img class="svg" src="img/phone.svg" alt="phone">
-			</div>
-		</a>
-		<a href="#" class="more">
-			<span>подробнее</span><i class="fa fa-angle-double-right"></i>
-		</a>
-	</div>
-	<!-- slider-item end -->
-	<!-- slider-item start -->
-	<div class=" top-catalog-slider__item ">
-		<div class="status">
+	<div class=" top-catalog-slider__item">
+		<div class="top-catalog-slider__item-status">
 			<img src="img/hit.png" alt="hit">
 		</div>
-		<div class="photo">
+		<div class="top-catalog-slider__item-photo">
 			<img src="img/generator.png"  alt="generator">
 		</div>
-		<div class="name">
+		<div class="top-catalog-slider__item-name">
 			<span>Генератор /<br> миниэлектростанция <br> ЕCMT 7000GV</span>
 		</div>
-		<div class="old-price">
+		<div class="top-catalog-slider__item-old-price">
 			<span>8 550 грн.</span>
 		</div>
-		<div class="price">
+		<div class="top-catalog-slider__item-price">
 			<span>6 900 грн.</span>
 		</div>
-		<a href="#" class="order">
+		<a href="#" class="top-catalog-slider__item-order">
 			Заказать
 			<div class="phone">
 				<img class="svg" src="img/phone.svg" alt="phone">
 			</div>
 		</a>
-		<a href="#" class="more">
+		<a href="#" class="top-catalog-slider__item-more">
 			<span>подробнее</span><i class="fa fa-angle-double-right"></i>
 		</a>
 	</div>
 	<!-- slider-item end -->
 	<!-- slider-item start -->
-	<div class=" top-catalog-slider__item ">
-		<div class="status">
-			<img src="img/akcii.png" alt="akcii">
+	<div class=" top-catalog-slider__item">
+		<div class="top-catalog-slider__inner-wrap clearfix">
+			<div class="top-catalog-slider__item-status">
+				<img src="img/hit.png" alt="hit">
+			</div>
+			<div class="top-catalog-slider__item-photo">
+				<img src="img/generator.png"  alt="generator">
+			</div>
+			<div class="top-catalog-slider__item-name">
+				<span>Генератор /<br> миниэлектростанция <br> ЕCMT 7000GV</span>
+			</div>
+			<div class="top-catalog-slider__item-old-price">
+				<span>8 550 грн.</span>
+			</div>
+			<div class="top-catalog-slider__item-price">
+				<span>6 900 грн.</span>
+			</div>
+			<a href="#" class="top-catalog-slider__item-order">
+				Заказать
+				<div class="phone">
+					<img class="svg" src="img/phone.svg" alt="phone">
+				</div>
+			</a>
+			<a href="#" class="top-catalog-slider__item-more">
+				<span>подробнее</span><i class="fa fa-angle-double-right"></i>
+			</a>
 		</div>
-		<div class="photo">
+	</div>
+	<!-- slider-item end -->
+	<!-- slider-item start -->
+	<div class=" top-catalog-slider__item">
+		<div class="top-catalog-slider__item-status">
+			<img src="img/hit.png" alt="hit">
+		</div>
+		<div class="top-catalog-slider__item-photo">
 			<img src="img/generator.png"  alt="generator">
 		</div>
-		<div class="name">
+		<div class="top-catalog-slider__item-name">
 			<span>Генератор /<br> миниэлектростанция <br> ЕCMT 7000GV</span>
 		</div>
-		<div class="old-price">
+		<div class="top-catalog-slider__item-old-price">
 			<span>8 550 грн.</span>
 		</div>
-		<div class="price">
+		<div class="top-catalog-slider__item-price">
 			<span>6 900 грн.</span>
 		</div>
-		<a href="#" class="order">
+		<a href="#" class="top-catalog-slider__item-order">
 			Заказать
 			<div class="phone">
 				<img class="svg" src="img/phone.svg" alt="phone">
 			</div>
 		</a>
-		<a href="#" class="more">
+		<a href="#" class="top-catalog-slider__item-more">
 			<span>подробнее</span><i class="fa fa-angle-double-right"></i>
 		</a>
 	</div>
 	<!-- slider-item end -->
+
+	
 </div>
 <!-- top-catalog-slider end -->
